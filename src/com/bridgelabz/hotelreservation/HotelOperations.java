b94d0fda769b2bc68dc93c4adb2f4fc95a9dc2c3
AdityaS98 <adityasharmalko1998@gmail.com>
2023-10-29 20:12:58 +0530

    Added: A java program to display welcome message

---
+++ src/com/bridgelabz/hotelreservation/HotelOperations.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.hotelreservation;
+
+public class HotelOperations {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the hotel reservation system");
+    }
+}
